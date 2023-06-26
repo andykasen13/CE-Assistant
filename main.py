a1444520d9ab915fb19e539419d56e2751d458b2
andykasen13 <andrewgarcha@gmail.com>
2023-06-26 15:20:46 -0400

    gyu

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from asyncio import Event, wait
 from datetime import datetime, timedelta
+import time
 import subprocess
 import discord
 from discord import app_commands
@@ -73,17 +74,21 @@ async def roll_command(interaction, event: str) -> None:
         while userInfo['users'][i]['ID'] != interaction.user.id :
             i += 1
         
-        # userInfo['users'][i]['current_rolls'].append({"event_name" : "One Hell of a Day", "games" : [{"name" : game, "completed" : False}], "end_time" : "" + datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        userInfo['users'][i]['current_rolls'].append({"event_name" : "One Hell of a Day", "games" : [{"name" : embed.title, "completed" : False}], "end_time" : "" + datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
 
         with open('info.json', 'w') as f :
             json.dump(userInfo, f, indent=4)
 
+        # TODO: make sure you indicate if the game is on sale
+        # TODO: think about switching the icon of 
+        # the /roll command to the user's avatar
+
         embed.add_field(name="Roll Requirements", value = 
-            "You have one day to complete " + game + "."    
-            + "\nMust be completed by " + (datetime.now()+timedelta(1)).strftime("%B %d, %Y at %I:%M:%S %p.") 
-            + "\nOne Hell of a Day has two week cooldown."
-            + "\nCooldown ends on " + (datetime.now()+timedelta(14)).strftime("%B %d, %Y at %I:%M:%S %p.")
-            + "\n[insert link to cedb.me page]", inline=False)
+            "You have one day to complete " + embed.title + "."    
+            + "\nMust be completed by <t:" + str(int(time.mktime((datetime.now()+timedelta(1)).timetuple())))
+            + ">\nOne Hell of a Day has two week cooldown."
+            + "\nCooldown ends on <t:" + str(int(time.mktime((datetime.now()+timedelta(14)).timetuple())))
+            + ">\n[insert link to cedb.me page]", inline=False)
         embed.set_author(name="ONE HELL OF A DAY", url="https://example.com")
 
         # Finally, send the embed
@@ -106,8 +111,11 @@ async def roll_command(interaction, event: str) -> None:
 # ----------------MY_ROLLS COMMAND ----------------------- # 
 # -------------------------------------------------------- #
 @tree.command(name="check_rolls", description="Check the active rolls of anyone on the server", guild=discord.Object(id=guildID))
-async def checkRolls(interaction, user: discord.Member) :
+async def checkRolls(interaction, user: discord.Member=None) :
     await interaction.response.defer()
+
+    if user is None :
+        user = interaction.user
 
     #open the json file and get the data
     with open('info.json', 'r') as f :
@@ -115,14 +123,13 @@ async def checkRolls(interaction, user: discord.Member) :
 
     #iterate through the json file until you find the
     #designated user
-    i = 0
-    while userInfo['users'][i]['ID'] != user.id :
-        if(i + 1 == len(userInfo['users'])) : return await interaction.followup.send("This user does not exist.")
-        else: i += 1
+    userNum = 0
+    while userInfo['users'][userNum]['ID'] != user.id :
+        if(userNum + 1 == len(userInfo['users'])) : return await interaction.followup.send("This user does not exist.")
+        else: userNum += 1
 
-    print(i)
-
-    checkrollstr = ""
+    currentrollstr = ""
+    completedrollstr = ""
 
     # TODO: make sure that if the roll is empty,
     # you don't do all this or it will error a lot!
@@ -132,26 +139,48 @@ async def checkRolls(interaction, user: discord.Member) :
     # it to include objectives in this, and if they are, 
     # how to show it.
 
-    # TODO: you forgot the end date silly
+    # (done) TODO: you forgot the end date silly
 
     # TODO: co-op rolls :weary: 😩
 
-    stupidSpaceIndent = 0
-
-    for x in userInfo['users'][i]['current_rolls'] :
-        stupidSpaceIndent += 1
-        checkrollstr = checkrollstr + "-" + x['event_name'] + ":\n"
+    for x in userInfo['users'][userNum]['current_rolls'] :
+        end_time = time.mktime(datetime.strptime(str(x['end_time']), "%Y-%m-%d %H:%M:%S").timetuple())
+        currentrollstr = currentrollstr + "- __" + x['event_name'] + "__ (complete by <t:" + str(int(end_time)) + ">):\n"
+        gameNum = 1
         for y in x['games'] :
-            checkrollstr = checkrollstr + ("  " * (stupidSpaceIndent)) + str(i+1) + ". " + y['name'] + " (completed: " + str(y['completed']) + ")\n"
+            if(y['completed']) :
+                currentrollstr += (" " + str(gameNum) + ". " + y['name'] + " (completed)\n")
+            else : currentrollstr += (" " + str(gameNum) + ". " + y['name'] + " (not completed)\n")
+            gameNum += 1
+    
+    if(currentrollstr == "") :
+        currentrollstr = f"{user.name} has no current rolls."
 
+    for x in userInfo['users'][userNum]['completed_rolls'] :
+        end_time = time.mktime(datetime.strptime(str(x['completed_time']), "%Y-%m-%d %H:%M:%S").timetuple())
+        completedrollstr += "- __" + x['event_name'] + "__ (completed on <t:" + str(int(end_time)) + ">):\n"
+        gameNum = 0
+        for y in x['games'] :
+            completedrollstr += (" " + str(gameNum) + ". " + y['name'] + "\n")
+            gameNum += 1
+    
+    if(completedrollstr == "") :
+        completedrollstr = f"{user.global_name} has no completed rolls."
+        
+
+        
     embed = discord.Embed(
     colour=0x000000,
     timestamp=datetime.now()
     )
 
+
     embed.add_field(name="User", value = "<@" + str(user.id) + ">", inline=False)
-    embed.add_field(name="Current Rolls", value=checkrollstr, inline=False)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/639112509445505046/891449764787408966/challent.jpg")
+    embed.add_field(name="Current Rolls", value=currentrollstr, inline=False)
+    embed.add_field(name="Completed Rolls", value=completedrollstr, inline=False)
+    embed.set_thumbnail(url=user.avatar.url)
+    embed.set_footer(text="CE Assistant",
+        icon_url="https://cdn.discordapp.com/attachments/639112509445505046/891449764787408966/challent.jpg")
 
     await interaction.followup.send(embed=embed)
 
@@ -190,6 +219,14 @@ async def steam_command(interaction, game_name: str):
 
     # Get the embed
     embed = getEmbed(game_name, interaction.user.id)
+
+    embed.set_author(name="REQUESTED STEAM GAME INFO ON '" + game_name + "'")
+    embed.remove_field(1)
+    embed.add_field(name="Requested by", value="<@" + str(interaction.user.id) + ">", inline=True)
+    embed.add_field(name="CE Status", value="Not on CE / x Points", inline=True)
+    embed.add_field(name="CE Owners", value="[insert]", inline=True)
+    embed.add_field(name="CE Completions", value="[insert]", inline=True)
+ 
 
     # Finally, send the embed
     await interaction.followup.send(embed=embed)
@@ -239,12 +276,8 @@ def getEmbed(game_name, authorID):
     gameTitle = jsonData[correctAppID]['data']['name']
     imageLink = jsonData[correctAppID]['data']['header_image']
     gameDescription = jsonData[correctAppID]['data']['short_description']
-    print(jsonData[correctAppID]['data']['is_free'])
-    print(correctAppID)
-    print(jsonData)
     if(jsonData[correctAppID]['data']['is_free']) :
         gamePrice = "Free"
-        print("is free")
     else: gamePrice = jsonData[correctAppID]['data']['price_overview']['final_formatted']
     gameNameWithLinkFormat = game_name.replace(" ", "_")
 
