92d842e6fd351635ec4644a3d8f30e34a8ffdd66
TheronBoozer <theronboozer@westminster.net>
2023-07-08 18:31:44 -0400

    fully scraped one page

--- scraping.py
+++ scraping.py
@@ -9,7 +9,7 @@ async def get_games():
     driver.get("https://cedb.me/games")
     games = await asyncio.wait_for(tag(driver), timeout=10)
     with open('database.json', 'w') as f :
-        json.dump(games, f, indent=1)
+        json.dump(games, f, indent=4)
 
 
 
@@ -18,23 +18,30 @@ async def tag(driver):
     games = {}
     while(please):
         names = driver.find_elements(By.TAG_NAME, "h2")
-        id = driver.find_elements(By.CSS_SELECTOR, ":link")
-        
+
         if len(names)<32:
             continue
         for plant in names:
             if not (plant.text and plant.text.strip()):
                 continue
         please = False
-    
+
+
+    id = driver.find_elements(By.CSS_SELECTOR, ":link")
+    tier_and_genre = driver.find_elements(By.CLASS_NAME, "bp4-text-overflow-ellipsis")
+    buttons = driver.findelements(By.TAG_NAME, "button")
+
+
     id = id[7::2]
     names = names[::2]
+    tiers = tier_and_genre[14::5]
+    genres = tier_and_genre[15::5]
     
     for i in range(0, len(id)):
-        print(id[i].get_attribute("href")[21::] + " " + names[i].text)
         games[names[i].text] = {
-            "CE ID" : id[i].get_attribute("href")[21::]
+            "CE ID" : id[i].get_attribute("href")[21::],
+            "tier" : tiers[i].text,
+            "genre" : genres[i].text
         }
-
 
     return games
