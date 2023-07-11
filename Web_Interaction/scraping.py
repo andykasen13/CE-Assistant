e5cdb5933627d3c46b04d6c219a75794e98aab07
andykasen13 <andrewgarcha@gmail.com>
2023-07-10 21:46:38 -0400

    database should work?

--- Web_Interaction/scraping.py
+++ Web_Interaction/scraping.py
@@ -5,6 +5,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import psutil
+import ftfy
 
 # Grab information from json file
 with open('Jasons/secret_info.json') as f :
@@ -14,15 +15,20 @@ steam_api_key = localJSONData['steam_API_key']
 
 
 def get_games():
-    options = webdriver.ChromeOptions() 
-    options.add_argument("--ignore-certificate-errors")
-    driver = webdriver.Chrome(options=options)
 
-    #driver = webdriver.Chrome()
+    ram_before = psutil.virtual_memory()[3]/1000000000
+    time_before = datetime.now()
+    driver = webdriver.Chrome()
     driver.get("https://cedb.me/games")
     # game_list(driver)
     game_data(driver)
-    print(get_objectives("[NINJA GAIDEN: Master Collection] NINJA GAIDEN Σ", 'f722baf5-4c1a-4fa3-9482-5ce6db203c73', driver))
+    # print(get_achievements("999990"))
+    # print(get_objectives("021b06b7-a5b5-4a54-bf45-36ed9037ee1d", driver))
+    ram_after = psutil.virtual_memory()[3]/1000000000
+    time_after = datetime.now()
+    print('ram usage (GB): ' + str(ram_after-ram_before))
+    print('time taken (s):' + str(time_after-time_before))
+    
 
 
 
@@ -37,13 +43,7 @@ def game_list(driver):
             names = driver.find_elements(By.TAG_NAME, "h2")[::2]
 
         ids = driver.find_elements(By.CSS_SELECTOR, ":link")[7::2]
-        tier_and_genre = driver.find_elements(By.CLASS_NAME, "bp4-text-overflow-ellipsis")
         button = driver.find_elements(By.TAG_NAME, "button")[30]
-
-
-        # tiers = tier_and_genre[14::5]
-        # genres = tier_and_genre[15::5]
-        # tier_and_genre=None
 
         ignore = [
             "- Challenge Enthusiasts -",
@@ -56,11 +56,9 @@ def game_list(driver):
                 continue
             games[names[i].text] = {
                 "CE ID" : ids[i].get_attribute("href")[21::],
-                # "tier" : tiers[i].text,
-                # "genre" : genres[i].text,
             }
 
-        button_enabled=False
+        # button_enabled=False
         if(not button.is_enabled()):
             print("done")
             button_enabled=False
@@ -77,9 +75,9 @@ def game_list(driver):
 
 def game_data(driver):
     games = json.loads(open("./Jasons/database_name.json").read())
-    print(games)
     for game in games:
-        data = get_objectives(game, games[game]["CE ID"], driver)
+        print(game)
+        data = get_objectives(games[game]["CE ID"], driver)
         for dat in data:
             games[game][dat] = data[dat]
 
@@ -87,7 +85,7 @@ def game_data(driver):
         json.dump(games, f, indent=4)
 
 
-def get_objectives(name, id, driver):
+def get_objectives(id, driver):
     url = "https://cedb.me/game/" + id
     driver.get(url)
     
@@ -98,84 +96,57 @@ def get_objectives(name, id, driver):
         except:
             continue
     tier_and_genre = driver.find_elements(By.CLASS_NAME, "bp4-fill")
+    app_id = driver.find_element(By.CLASS_NAME, 'no-decoration').get_attribute("href")[34::]
     tier = tier_and_genre[0].text
     genre = tier_and_genre[1].text
 
 
     objectives_lst = objectives_string.split("\n")
     primary_objectives = {}
-    all_achievements = get_achievements(name)
-
-    print(objectives_lst)
+    all_achievements = get_achievements(app_id)
     while(len(objectives_lst) > 0):
         achievements = []
         intermediate = {}
+
         title = objectives_lst.pop(0)
         intermediate["Point Value"] = objectives_lst.pop(0)
         intermediate["Description"] = objectives_lst.pop(0)
         if(len(objectives_lst) > 0 and objectives_lst[0] == "Achievements:"):
             objectives_lst.pop(0)
-            while(len(objectives_lst) > 0 and all_achievements.count(objectives_lst[0]) > 0):
-                if not(len(objectives_lst) > 3 and objectives_lst[2] == "Achievements:" or "Requirements:"):
+            while(len(objectives_lst) > 0 and all_achievements.count(objectives_lst[0].strip()) > 0):
+                if (not (len(objectives_lst) > 3 and (objectives_lst[3] == "Achievements:" or objectives_lst[3] == "Requirements:"))):
                     achievements.append(objectives_lst.pop(0))
-
+                else:
+                    break
             intermediate["Achievements"] = achievements
         if(len(objectives_lst) > 0 and objectives_lst[0] == "Requirements:"):
             objectives_lst.pop(0)
+
             intermediate["Requirements"] = objectives_lst.pop(0)
         primary_objectives[title] = intermediate
 
     full_game_info = {
+        "Steam ID" : app_id,
         "Tier" : tier,
         "Genre" : genre,
         "Objectives" : primary_objectives
     }
-    
+
     return full_game_info
 
 
 
 
-def get_achievements(game_name) :
-    # ---- GET APP ID ----
-    game_word_lst = game_name.split(" ")
-    for name in game_word_lst:
-        if len(name) != len(name.encode()):
-            game_word_lst.pop(game_word_lst.index(name))
-
-
-    searchable_game_name = " ".join(game_word_lst)
-
-    payload = {'term': searchable_game_name, 'f': 'games', 'cc' : 'us', 'l' : 'english'}
-    response = requests.get('https://store.steampowered.com/search/suggest?', params=payload)
-
-    divs = BeautifulSoup(response.text, features="html.parser").find_all('div')
-    ass = BeautifulSoup(response.text, features="html.parser").find_all('a')
-    options = []
-    for div in divs:
-        try:
-            if div["class"][0] == "match_name":
-                options.append(div.text)
-        except:
-            continue
-
-    correct_app_id = ass[0]['data-ds-appid']
-
-    for i in range(0, len(options)):
-        if game_name == options[i]:
-            correct_app_id = ass[i]['data-ds-appid']
-
-    #TODO: dap isn't working sorry theron
-
-    # --- DOWNLOAD JSON FILE ---
+def get_achievements(steam_id) :
 
     # Open and save the JSON data
-    payload = {'key' : steam_api_key, 'appid': correct_app_id, 'cc' : 'US'}
+    payload = {'key' : steam_api_key, 'appid': steam_id, 'cc' : 'US'}
     response = requests.get("https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?", params = payload)
     jsonData = json.loads(response.text)
 
     all_achievements = []
-    for achievement in jsonData['game']['availableGameStats']['achievements'] :
-        all_achievements.append(achievement['displayName'])
+    if 'availableGameStats' in jsonData['game'].keys() and 'achievements' in jsonData['game']['availableGameStats'].keys():
+        for achievement in jsonData['game']['availableGameStats']['achievements'] :
+            all_achievements.append(achievement['displayName'].strip())
     
     return all_achievements
