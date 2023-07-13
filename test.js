4351bc072030d442d0b8526adcdb79b492087e07
TheronBoozer <theronboozer@westminster.net>
2023-07-13 16:42:35 -0400

    ugh

--- test.js
+++ test.js
@@ -4,5 +4,13 @@ var Vibrant = require('node-vibrant')
 // TypeScript
 // import Vibrant = require('node-vibrant')
 
-Vibrant.from('Web_Interaction/header (1).jpg').getPalette()
+var vibrant = Vibrant.from('Web_Interaction/header (1).jpg').getPalette()
   .then((palette) => console.log(palette))
+
+var hsl = vibrant.getHSL()
+  , a = hsl[0]
+  , i = hsl[1]
+  , s = hsl[2]
+s = 1
+newColor = new ie.Swatch(se.hslToRgb(a, i, s), 0)
+console.log(newColor)
