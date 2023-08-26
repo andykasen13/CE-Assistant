7c8e9512181cbd2fb4a6499d08b5518e18fdd9f3
TheronBoozer <theronboozer@westminster.net>
2023-08-26 18:56:22 -0400

    started anew

--- Helper_Functions/Scheduler.py
+++ Helper_Functions/Scheduler.py
@@ -5,6 +5,15 @@ import datetime
 
 from Helper_Functions.end_time import roll_failed
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+sched = BackgroundScheduler()
+
+
+def test(this, that):
+    print(this + " and " + that)
+
 
 def get_tasks():
     users = json.loads(open("./Jasons/users2.json").read())
@@ -23,29 +32,45 @@ def get_tasks():
                 "User CE ID": user["CE ID"]
             }
 
-            datetimes.append(datetime.datetime.utcfromtimestamp(int(current_roll["End Time"])).time())
 
-    # with open("./Jasons/tasks.json", "w") as f:
-    #     json.dump(fin, f, indent=4)
 
-    print("Scheduled")
-    print(datetimes)
-    print(datetimes[0])
+    # # with open("./Jasons/tasks.json", "w") as f:
+    # #     json.dump(fin, f, indent=4)
 
-    return datetimes
+    # print("Scheduled")
+    # print(datetimes)
+    # print(datetimes[0])
+
+
+def create_schedule():
+                
+    #read roles from other thing then do shit with those
     
+    #         date_time = datetime.datetime.utcfromtimestamp(int(current_roll["End Time"]))
+    #         this = "this"
+    #         that = "that"
+    #         sched.add_job(test, 'date', run_date = date_time, args = [this,that])
+
+    # sched.start()
+    return
+
+# task_dict = json.loads(open("./Jasons/tasks.json").read())
+# task_list = get_tasks()
 
 
-task_dict = json.loads(open("./Jasons/tasks.json").read())
-task_list = get_tasks()
+
+# @tasks.loop(time=task_list)
+# async def process_schedule():
+#     current_time = int(time.time())
+#     current_dict = task_dict[current_time]
+
+#     print(current_dict['Event Name'])
+#     print(current_dict['User CE ID'])
+#     # roll_failed(task_list[current_time]['Event Name'], task_list[current_time]['User CE ID'])
 
 
 
-@tasks.loop(time=task_list)
-async def process_schedule():
-    current_time = int(time.time())
-    current_dict = task_dict[current_time]
 
-    print(current_dict['Event Name'])
-    print(current_dict['User CE ID'])
-    # roll_failed(task_list[current_time]['Event Name'], task_list[current_time]['User CE ID'])
+
+
+    
--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from selenium import webdriver
 from PIL import Image
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from Helper_Functions.Scheduler import get_tasks, process_schedule
+from Helper_Functions.Scheduler import get_tasks
 
 # --------- other file imports ---------
 from Web_Interaction.loopty_loop import master_loop, thread_curate
@@ -790,8 +790,7 @@ async def reroll(interaction : discord.Interaction, event : events_total) :
 async def on_ready():
     await tree.sync(guild=discord.Object(id=guild_ID))
     print("Ready!")
-    # await get_tasks()
-    await process_schedule.start()
+    get_tasks()
     await master_loop.start(client)
 
 client.run(discord_token)
