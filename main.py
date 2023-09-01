f6672f5b46928e122108c9ae29f5744c22a268e8
andy :) <andrewgarcha@gmail.com>
2023-09-01 18:16:03 -0400

    push again!

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ intents.message_content = True
 with open('Jasons/secret_info.json') as f :
     localJSONData = json.load(f)
 
-discord_token = localJSONData['other_discord_token']  
+discord_token = localJSONData['discord_token']  
 guild_ID = localJSONData['test_guild_ID']
 
 ce_mountain_icon = "https://cdn.discordapp.com/attachments/639112509445505046/891449764787408966/challent.jpg"
