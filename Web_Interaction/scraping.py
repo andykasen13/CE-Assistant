9e303a0b3c49b6d2366fc03c9990c72f9ab8f545
andy :) <andrewgarcha@gmail.com>
2023-09-01 22:19:07 -0400

    inshallah this shall actually work

--- Web_Interaction/scraping.py
+++ Web_Interaction/scraping.py
@@ -187,6 +187,7 @@ def game_list():
         # elif not game['name'] in list(new_data.keys()) and game['genreId'] != None:
         elif created_time > current_newest:
             print(game['name'])
+            if game['tier'] == 0 : continue
             get_image(number, game['id'], driver)
             new_game = get_game(game)
             new_data[game['name']] = new_game
