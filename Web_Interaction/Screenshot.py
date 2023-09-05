5951f17a40055305c78fa359b7197a865868fd5a
andy :) <andrewgarcha@gmail.com>
2023-09-04 23:01:58 -0400

    Update Screenshot.py

--- Web_Interaction/Screenshot.py
+++ Web_Interaction/Screenshot.py
@@ -129,7 +129,7 @@ class Screenshot:
                 self.hide_elements(driver, hide_elements)
                 print('15')
 
-                file_name = "part_{0}.png".format(part)
+                file_name = "./Pictures/part_{0}.png".format(part)
                 print('16')
                 driver.get_screenshot_as_file(file_name)
                 print('17')
