2551aa8cb07cd8e5bb23500a4186d28e83cf13ed
andy :) <andrewgarcha@gmail.com>
2023-09-05 17:12:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -111,6 +111,7 @@ async def help(interaction : discord.Interaction) :
 
 
         async def callback(self, interaction: discord.Interaction):
+            await interaction.response.defer()
             embed = self.get_embed()
             if self.values[0] == 'Rolls' or self.values[0] in list(roll_options.keys()):
                 await interaction.followup.edit_message(embed = embed, view=HelpSelectView(menu=rolls, message="Rolls", message_2=self.values[0]), message_id = interaction.message.id)
