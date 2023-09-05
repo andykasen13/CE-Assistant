c63de3405cdc5b1cf3c34a87ed762ee89412047f
andy :) <andrewgarcha@gmail.com>
2023-09-05 10:27:26 -0400

    Update Screenshot.py

--- Web_Interaction/Screenshot.py
+++ Web_Interaction/Screenshot.py
@@ -140,7 +140,7 @@ class Screenshot:
                 print("ss")
                 path.parent.mkdir(parents=True, exist_ok=True)
                 print('makedirs')
-                with open(path, "w+") as ss_file:
+                with open(path, "wb") as ss_file:
                     print('with open')
                     ss_file.write(ss)
                     print("Svreenshot saved")
