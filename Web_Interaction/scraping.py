8ed9c155ca3f333f3ab884609b23f93eaab8ddad
andy :) <andrewgarcha@gmail.com>
2023-09-13 17:26:25 -0400

    Update scraping.py

--- Web_Interaction/scraping.py
+++ Web_Interaction/scraping.py
@@ -848,8 +848,18 @@ def special_image(number, decimal, CE_ID, driver, objective_name):
     
     ob = Screenshot(bottom_right_y)
     im = ob.full_screenshot(driver, save_path=r'Pictures/', image_name="ss{}.png".format(str(number) + '-' + str(decimal)), is_load_at_runtime=True, load_wait_time=3, hide_elements=header_elements)
-    im = Image.open('/CE-Assistant/Pictures/ss{}.png'.format(str(number) + '-' + str(decimal)))
+    im = io.BytesIO(im)
+    im_image = Image.open(im)
+    im_image = im_image.crop((top_left_x, top_left_y, bottom_right_x, bottom_right_y))
+
+    imgByteArr = io.BytesIO()
+    im_image.save(imgByteArr, format='PNG')
+    final_im = imgByteArr.getvalue()
+    
+    return final_im
+
+    """im = Image.open('/CE-Assistant/Pictures/ss{}.png'.format(str(number) + '-' + str(decimal)))
     im = im.crop((top_left_x, top_left_y, bottom_right_x, bottom_right_y)) # defines crop points
     im.save('/CE-Assistant/Pictures/ss{}.png'.format(str(number) + '-' + str(decimal)))
 
-    return
+    return"""
