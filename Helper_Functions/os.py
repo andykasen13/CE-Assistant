35e1f51fbe06191a287a3d34f2b3c4a25f5d48d7
TheronBoozer <theronboozer@westminster.net>
2024-03-16 16:26:06 -0400

    pc restarts, writes batch file if needed, and boots code

--- Helper_Functions/os.py
+++ Helper_Functions/os.py
@@ -1,4 +1,29 @@
 import os
+import getpass
 
-async def restart():
+USER_NAME = getpass.getuser()
+bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+
+
+async def restart(file):
+    if not os.path.isfile(bat_path + "/open.bat"):
+        await add_to_startup(file)
     os.system("shutdown /r /t 0")
+
+
+async def add_to_startup(file, file_path=""):
+    
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(file))
+
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'''@echo off
+pip install discord selenium pillow requests bs4 apscheduler pymongo motor chromedriver_binary webdriver_manager
+cd %s
+python main.py
+pause''' % file_path)
+                       
+
+
+
+
