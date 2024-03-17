2685039862e74e611828ac48f046f84f8cd9d8fe
andy :) <andrewgarcha@gmail.com>
2024-03-17 16:30:02 -0400

    Update buttons.py

--- Helper_Functions/buttons.py
+++ Helper_Functions/buttons.py
@@ -129,11 +129,13 @@ async def get_genre_buttons(view : discord.ui.View, completion_time : int, price
         database_tier = await get_mongo("tier")
         steamhunters = await get_mongo('steamhunters')
         database_user = await get_mongo('user')
+        ce_id = await get_ce_id(user_id)
+
 
 
         i=0
         while i < num_of_games :
-            games.append(get_rollable_game(completion_time, price_limit, tier_number, specific_genre =genre_name, database_tier=database_tier, database_name=database_name, games=games, steamhunters=steamhunters, user_info=database_user))
+            games.append(get_rollable_game(completion_time, price_limit, tier_number, specific_genre =genre_name, database_tier=database_tier, database_name=database_name, games=games, steamhunters=steamhunters, user_info=database_user[ce_id]))
             i+=1
 
         embeds = create_multi_embed(event_name, time_limit, games, cooldown_time, interaction, database_name)
