19dbff3f1d1fa4cf7dd522af9435d7edb6d6e736
andy :) <andrewgarcha@gmail.com>
2024-03-19 00:43:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -837,13 +837,16 @@ class RequestCEGame(discord.ui.Modal):
             valid = True
             if game_name != "" and not database_name[game_id]['Name'].lower()[0:len(game_name)] == game_name.lower() : valid = False
             if tier != None :
+                if tier == 6 or tier == 7:
+                    total_points = 0
+                    for obj_id in database_name[game_id]['Primary Objectives'] : total_points += database_name[game_id]['Primary Objectives'][obj_id]['Point Value']
+                    if tier == 6 and (total_points < 500 or total_points >= 1000) : valid = False
+                    elif tier == 7 and total_points >= 1000 : valid = False 
                 if not database_name[game_id]['Tier'] == f"Tier {tier}" and tier != 6 and tier != 7 : valid = False
-            if points != None or tier == 6 or tier == 7:
+            if points != None:
                 total_points = 0
                 for obj_id in database_name[game_id]['Primary Objectives'] : total_points += database_name[game_id]['Primary Objectives'][obj_id]['Point Value']
-                if points != None and total_points < min_points or total_points > max_points : valid = False
-                if tier == 6 and (total_points < 500 or total_points >= 1000) : valid = False
-                elif tier == 7 and total_points >= 1000 : valid = False 
+                if total_points < min_points or total_points > max_points : valid = False
             if genre != None and not database_name[game_id]['Genre'] == genre : valid = False
             if owned != None and not game_id in database_user[ce_id]['Owned Games'] : valid = False
             if valid : game_list.append(game_id)
